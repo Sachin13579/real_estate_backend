977be918987aafbe7c1200a81753c8f3dac9722f
Puneet Srivastava <srivastavap714@gmail.com>
2024-01-16 17:23:09 +0530

    user model added

---
+++ model/user.model.js
@@ -0,0 +1,67 @@
+import mongoose from 'mongoose'
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    phone: {
+      type: Number,
+      required: true,
+    },
+    password: {
+      type: String,
+    },
+   otp:{
+    type: Number,
+    required: true,
+   },
+   
+    address: {
+      type: String,
+    },
+    city: {
+        type: String,
+        require: true,
+      },
+      state: {
+        type: String,
+        require: true,
+      },
+      social:{
+         type: url,
+      },
+      twitter:{
+        type: url,
+      },
+      linkdin:{
+        type: url,
+      },
+    property: [
+        {
+          type: mongoose.ObjectId,
+          ref: 'propertyList',
+        },
+      ],
+      role: {
+        type: String,
+        default: 'superadmin',
+      },
+    
+    coassignTo: {
+      type: mongoose.ObjectId,
+      ref: 'adminUser',
+    },
+  
+   
+  },
+
+  {timestamps: true}
+)
+
+export default mongoose.model('user', userSchema)
